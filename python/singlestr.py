5ac3fca507a0cdf17dddbc565be180d37aa0ecfb
AKSHAY UNNI M V <144668312+akshayunnimv@users.noreply.github.com>
2023-10-16 23:32:25 +0530

    Update singlestr.py

--- python/singlestr.py
+++ python/singlestr.py
@@ -1,6 +1,3 @@
 str1=str(input('enter the 1st string:'))
 str2=str(input('enter the 2nd string:'))
-print("the entered string is",str1+' '+str2)
-s=str2[:1]+str1[1:]
-s1=str1[:1]+str2[1:]
-print('the new string is',s+' '+s1)
+print('the new string is',str2[0]+str1[1:]+' '+str1[0]+str2[1:])
